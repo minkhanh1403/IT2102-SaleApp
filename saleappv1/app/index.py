5536b58f4fdfc5af0f5db5228fbcb2d6e98a5df0
minhkhanh1403 <minhkhanh14032002@gmail.com>
2023-10-31 14:31:29 +0700

    addmysql

--- saleappv1/app/index.py
+++ saleappv1/app/index.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from flask import render_template, request
 import  dem
+from saleappv1.app import app
 
-app = Flask(__name__)
 
 @app.route('/')
 def index():
